f622c5848f3ff91a72b400b0f176d5c2e154e757
augustoceglia13-arch <augustoceglia13@gmail.com>
2026-02-24 12:07:33 +0100

    aggiunta delle query terminate

---
+++ Database_Marcatori/SQLQuery1.sql
@@ -0,0 +1,10 @@
+SELECT TOP (1000) [Id]
+      ,[Nome]
+  FROM [Marcatori].[dbo].[Continenti]
+  INSERT INTO [dbo].[Continenti](Id,Nome)
+  VALUES 
+  (1,'Americhe'),
+  (2,'Europa'),
+  (3,'Oceania'),
+  (4,'Asia'),
+  (5,'Africa')
