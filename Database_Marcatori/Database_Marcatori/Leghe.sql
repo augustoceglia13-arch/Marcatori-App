b79ff3050d2421f5766dd4ea3935b0256906753d
augustoceglia13-arch <augustoceglia13@gmail.com>
2026-03-02 16:15:43 +0100

    RIMOZIONE COLONNA SQUADRE DALLA TABELLA LEGHE

--- Database_Marcatori/Database_Marcatori/Leghe.sql
+++ Database_Marcatori/Database_Marcatori/Leghe.sql
@@ -3,6 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Id_Nazione] INT NULL, 
     [Nome] VARCHAR(200) NULL, 
-    [Squadre] VARCHAR(200) NULL, 
     CONSTRAINT [FK_Leghe_Nazioni] FOREIGN KEY ([Id_Nazione]) REFERENCES [Nazioni]([Id])
 )
