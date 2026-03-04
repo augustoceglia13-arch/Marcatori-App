a9a919e658502cbeeadd0585c868a18e45c10828
augustoceglia13-arch <augustoceglia13@gmail.com>
2026-03-04 10:49:17 +0100

    Implementata connesione a db

--- Database_Marcatori/MarcatoriWebSite/Controllers/HomeController.cs
+++ Database_Marcatori/MarcatoriWebSite/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using System.Diagnostics;
+using Dapper;
 using MarcatoriWebSite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -10,23 +11,50 @@ namespace MarcatoriWebSite.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+           string connectionString = "Server=localhost;Database=Marcatori;TrustServerCertificate=true;Integrated Security = SSPI";
+           IEnumerable<Campionato> listaCampionati;
+           using (IDbConnection connection = new SqlConnection(connectionString))
+           {
+               string sql = @"
+SELECT TOP 5
+    IDNazione = N.Id,
+    Nazione = N.Nome,
+    IDLega = L.Id,
+    Lega = L.Nome,
+    NumPartite = COUNT(*)
+FROM 
+    [dbo].[Partite] P 
+    INNER JOIN [dbo].[Squadre] Sc ON P.Id_Squadra_Casa = SC.Id
+    INNER JOIN [dbo].[Squadre] SO ON P.Id_Squadra_Ospite= So.Id
+    INNER JOIN [dbo].[Leghe] L ON L.Id = SC.Id_Lega and L.Id = SO.Id_Lega
+    INNER JOIN [dbo].[Nazioni] N ON N.Id = L.Id_Nazione
+GROUP BY
+	N.Id,
+	L.Id,
+	N.Nome,
+	L.Nome
+ ORDER BY COUNT(*) DESC";
+
+               connection.Open();
+               listaCampionati = connection.Query<Campionato>(sql);
+           }
+            return View(listaCampionati);
         }
         public ActionResult Campionati()
         {
-            string connectionString = "Server=localhost;Database=Marcatori;Trusted_Connection=True;uid=sa;password=lapassword";
-            IEnumerable<Campionato> listaCampionati;
-            using (IDbConnection connection = new SqlConnection(connectionString))
-            {
-                string sql = @"SELECT 
-                                  Id,
-                               Nome,
-                               DataInizio,
-                               DataFine
-                           FROM Campionati";
-                connection.Open();
-                listaCampionati = connection.Query<Campionato>(sql);
-            }
+            //string connectionString = "Server=localhost;Database=Marcatori;Trusted_Connection=True;uid=sa;password=lapassword";
+            //IEnumerable<Campionato> listaCampionati;
+            //using (IDbConnection connection = new SqlConnection(connectionString))
+            //{
+            //    string sql = @"SELECT 
+            //                      Id,
+            //                   Nome,
+            //                   DataInizio,
+            //                   DataFine
+            //               FROM Campionati";
+            //    connection.Open();
+            //    listaCampionati = connection.Query<Campionato>(sql);
+            //}
             return View();
         }
 
---
+++ Database_Marcatori/MarcatoriWebSite/Models/Campionato.cs
@@ -0,0 +1,11 @@
+﻿namespace MarcatoriWebSite.Models
+{
+    public class Campionato
+    {
+        public int IDNazione { get; set; }
+        public string Nazione { get; set; }
+        public int IDLega { get; set; }
+        public string Lega { get; set; }
+        public int NumPartite { get; set; }
+    }
+}
